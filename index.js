8227a7b4c034269f742bc8114de5f9bd0f221ce5
Mark Louisse Jardiniano <112992310+MarkLouisseJardiniano@users.noreply.github.com>
2024-07-03 09:40:06 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -117,4 +117,14 @@ app.post("/login", async (req, res) => {
   }
 });
 
+// Endpoint to retrieve all users
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to retrieve users" });
+  }
+});
+
 module.exports.handler = serverless(app);
